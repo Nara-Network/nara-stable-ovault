9de789e0185482adc37e2c63fa69e3cae2540f0c
Teodorus Nathaniel <tnathanielk@gmail.com>
2025-12-11 00:13:37 +0700

    feat: update some contracts to be upgradable

--- contracts/narausd-plus/NaraUSDPlusSilo.sol
+++ contracts/narausd-plus/NaraUSDPlusSilo.sol
@@ -2,20 +2,64 @@
 pragma solidity ^0.8.22;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/access/Ownable2StepUpgradeable.sol";
 
 /**
  * @title NaraUSDPlusSilo
  * @notice The Silo allows to store staking vault tokens (NaraUSD+) during the stake cooldown process.
+ * @dev This contract is upgradeable using UUPS proxy pattern
  */
-contract NaraUSDPlusSilo {
+contract NaraUSDPlusSilo is Initializable, UUPSUpgradeable, Ownable2StepUpgradeable {
     /// @notice Error emitted when the staking vault is not the caller
     error OnlyStakingVault();
+    error InvalidZeroAddress();
 
-    address immutable STAKING_VAULT;
-    IERC20 immutable TOKEN;
+    address public STAKING_VAULT;
+    IERC20 public TOKEN;
 
-    constructor(address stakingVault, address token) {
+    /// @custom:oz-upgrades-unsafe-allow constructor
+    constructor() {
+        _disableInitializers();
+    }
+
+    /**
+     * @notice Initialize the contract
+     * @param stakingVault The staking vault address that can withdraw
+     * @param token The token address (NaraUSD+)
+     */
+    function initialize(address stakingVault, address token) public initializer {
+        if (stakingVault == address(0) || token == address(0)) revert InvalidZeroAddress();
+
+        __Ownable2Step_init();
+        __UUPSUpgradeable_init();
+
         STAKING_VAULT = stakingVault;
+        TOKEN = IERC20(token);
+    }
+
+    /**
+     * @notice Authorize upgrade (UUPS pattern)
+     * @dev Only owner can authorize upgrades
+     */
+    function _authorizeUpgrade(address newImplementation) internal override onlyOwner {}
+
+    /**
+     * @notice Update staking vault address (only owner, for deployment flexibility)
+     * @param stakingVault The new staking vault address
+     */
+    function setStakingVault(address stakingVault) external onlyOwner {
+        if (stakingVault == address(0)) revert InvalidZeroAddress();
+        STAKING_VAULT = stakingVault;
+    }
+
+    /**
+     * @notice Update token address (only owner, for deployment flexibility)
+     * @param token The new token address
+     */
+    function setToken(address token) external onlyOwner {
+        if (token == address(0)) revert InvalidZeroAddress();
         TOKEN = IERC20(token);
     }
 
--- contracts/narausd/NaraUSDRedeemSilo.sol
+++ contracts/narausd/NaraUSDRedeemSilo.sol
@@ -2,20 +2,64 @@
 pragma solidity ^0.8.22;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/access/Ownable2StepUpgradeable.sol";
 
 /**
  * @title NaraUSDRedeemSilo
  * @notice Holds locked NaraUSD tokens during redemption queue period
+ * @dev This contract is upgradeable using UUPS proxy pattern
  */
-contract NaraUSDRedeemSilo {
+contract NaraUSDRedeemSilo is Initializable, UUPSUpgradeable, Ownable2StepUpgradeable {
     /// @notice Error when caller is not the vault
     error OnlyVault();
+    error InvalidZeroAddress();
 
-    address immutable VAULT;
-    IERC20 immutable naraUSD;
+    address public VAULT;
+    IERC20 public naraUSD;
 
-    constructor(address vault, address narausd) {
+    /// @custom:oz-upgrades-unsafe-allow constructor
+    constructor() {
+        _disableInitializers();
+    }
+
+    /**
+     * @notice Initialize the contract
+     * @param vault The vault address that can withdraw
+     * @param narausd The NaraUSD token address
+     */
+    function initialize(address vault, address narausd) public initializer {
+        if (vault == address(0) || narausd == address(0)) revert InvalidZeroAddress();
+
+        __Ownable2Step_init();
+        __UUPSUpgradeable_init();
+
         VAULT = vault;
+        naraUSD = IERC20(narausd);
+    }
+
+    /**
+     * @notice Authorize upgrade (UUPS pattern)
+     * @dev Only owner can authorize upgrades
+     */
+    function _authorizeUpgrade(address newImplementation) internal override onlyOwner {}
+
+    /**
+     * @notice Update vault address (only owner, for deployment flexibility)
+     * @param vault The new vault address
+     */
+    function setVault(address vault) external onlyOwner {
+        if (vault == address(0)) revert InvalidZeroAddress();
+        VAULT = vault;
+    }
+
+    /**
+     * @notice Update naraUSD token address (only owner, for deployment flexibility)
+     * @param narausd The new naraUSD token address
+     */
+    function setNaraUSD(address narausd) external onlyOwner {
+        if (narausd == address(0)) revert InvalidZeroAddress();
         naraUSD = IERC20(narausd);
     }
 
@@ -29,4 +73,3 @@ contract NaraUSDRedeemSilo {
         naraUSD.transfer(to, amount);
     }
 }
-
