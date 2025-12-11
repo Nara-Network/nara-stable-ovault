8efbb80cb395c713b5af79f5aa013cff1b5c2dfe
Teodorus Nathaniel <tnathanielk@gmail.com>
2025-12-11 13:06:21 +0700

    chore: remove unused import

--- contracts/narausd/NaraUSDOFTAdapter.sol
+++ contracts/narausd/NaraUSDOFTAdapter.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity ^0.8.22;
 
-import { OwnableUpgradeable } from "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 import { Initializable } from "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import { UUPSUpgradeable } from "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
 import { OFTAdapterUpgradeable } from "@layerzerolabs/oft-evm-upgradeable/contracts/oft/OFTAdapterUpgradeable.sol";
